31abb3ddb1b63fba8ab6b5cf5e2049cee6763e57
junielism <Jiratheep.s@ku.th>
2017-03-06 22:35:01 +0700

    ส่งการบ้านครับ c0004.c

---
+++ challenges-solved/5910406094_c0004.c
@@ -0,0 +1,34 @@
+// code c0004.md Jiratheep Sadakorn 5910406094
+#include <stdio.h>
+int main(){
+	float f,s,S;
+	char cha;
+	printf("What do you want to find?\n");
+	printf("Wanna find : ");
+	scanf("%c",&cha);
+	if(cha == 'f'){
+	printf("Input s = ");
+	scanf("%f",&s);
+	printf("Input S = ");
+	scanf("%f",&S);
+	f = 1/(1/s+1/S);
+	printf("%c = %.1f",cha,f);
+	}
+	else if(cha == 's'){
+	printf("Input f = ");
+	scanf("%f",&f);
+	printf("Input S = ");
+	scanf("%f",&S);
+	s=1.0/((S-f)/(f*S));
+	printf("%c = %.1f",cha,s);
+	}
+	else if(cha == 'S'){
+	printf("Input f = ");
+	scanf("%f",&f);
+	printf("Input s = ");
+	scanf("%f",&s);
+	S = 1.0/((s-f)/(f*s));
+	printf("%c = %.1f",cha,S);
+	}
+
+}
