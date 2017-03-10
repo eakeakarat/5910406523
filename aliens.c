4d5f09b209168c5a556191783be0991e62127845
robinker <parkershock@gmail.com>
2017-03-10 23:31:16 +0700

    Add files via upload

---
+++ aliens.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    unsigned long long  n, i, a_4, sum, day[4] = {1, 0, 0, 0};
+ 
+    printf("Input: ");
+    scanf("%llu", &n);
+    //give birth;
+    
+
+    for(i=0; i<n; i++)
+    {
+    	a_4 = day[3];
+        day[3] = day[2];
+        day[2] = day[1];
+        day[1] = day[0];
+        day[0] = 8*day[1] + 4*day[2] + 2*day[3] + a_4;
+        
+    }
+    sum=day[0]+day[1]+day[2]+day[3];
+    printf("Output: %llu", sum);
+}
