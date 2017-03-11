7f514fd7c77c56efd58515de96f8541aa832b02e
Saac <saacsos@gmail.com>
2017-03-11 08:13:31 +0700

    Mission Failed

---
+++ challenges-solved/c0002/c0002-s0001.c
@@ -0,0 +1,37 @@
+//c0002
+// by @Tanamet
+// status: Mission failed
+#include <stdio.h>
+#include <String.h>
+#define True 1
+#define False 0
+
+int main(){
+    int running=True;
+    double n,x,y;
+    char command[5];
+    printf("test\n");
+    scanf("%lf",&n);
+    x = n; y = n;
+    while(running){
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i==y && j==x)
+                putchar('X');
+                else
+                putchar('=');
+            }
+            putchar('\n');
+        }
+        scanf("%s",&command);
+            if (strcmp(command,"up")==0) y-=(y<2?0:1);
+            else if (strcmp(command,"down")==0) y+=(y>4?0:1);
+            else if (strcmp(command,"left")==0) x-=(x<2?0:1);
+            else if (strcmp(command,"right")==0) x+=(x>4?0:1);
+            else if (strcmp(command,"stop")==0) running=False;
+            else printf("Command not found\n");
+    }
+  return 0;
+}
